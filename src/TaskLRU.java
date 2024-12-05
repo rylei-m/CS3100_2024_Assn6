b960265696b6a345a152d13ec8c8d85e70dee239
Rylei Mindrum <rymindrum@gmail.com>
2024-12-04 23:45:48 -0700

    LRU and MRU

---
+++ src/TaskLRU.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class TaskLRU implements Runnable {
+    private final int[] sequence;
+    private final int maxMemoryFrames;
+    private final int[] pageFaults;
+
+    public TaskLRU(int[] sequence, int maxMemoryFrames, int maxPageReference, int[] pageFaults) {
+        this.sequence = sequence;
+        this.maxMemoryFrames = maxMemoryFrames;
+        this.pageFaults = pageFaults;
+    }
+
+    @Override
+    public void run() {
+        LinkedHashSet<Integer> memory = new LinkedHashSet<>();
+        int faults = 0;
+
+        for (int page : sequence) {
+            if (!memory.contains(page)) {
+                faults++;
+                if (memory.size() == maxMemoryFrames) {
+                    // Remove the least recently used (first element)
+                    int lru = memory.iterator().next();
+                    memory.remove(lru);
+                }
+            } else {
+                // Re-accessed page; remove and re-insert to update order
+                memory.remove(page);
+            }
+            memory.add(page);
+        }
+        pageFaults[maxMemoryFrames] = faults;
+    }
+}
--- src/TaskMRU.java
+++ src/TaskMRU.java
@@ -1,1 +1,37 @@
+import java.util.*;
 
+public class TaskMRU implements Runnable {
+    private final int[] sequence;
+    private final int maxMemoryFrames;
+    private final int[] pageFaults;
+
+    public TaskMRU(int[] sequence, int maxMemoryFrames, int maxPageReference, int[] pageFaults) {
+        this.sequence = sequence;
+        this.maxMemoryFrames = maxMemoryFrames;
+        this.pageFaults = pageFaults;
+    }
+
+    @Override
+    public void run() {
+        LinkedHashSet<Integer> memory = new LinkedHashSet<>();
+        int faults = 0;
+        int mostRecentlyUsed = -1; // Tracks the most recently used page
+
+        for (int page : sequence) {
+            if (!memory.contains(page)) {
+                faults++;
+                if (memory.size() == maxMemoryFrames) {
+                    // Evict the most recently used page
+                    memory.remove(mostRecentlyUsed);
+                }
+                memory.add(page);
+            } else {
+                // Re-accessed page; just update the MRU tracker
+                memory.remove(page);
+                memory.add(page);
+            }
+            mostRecentlyUsed = page; // Update MRU
+        }
+        pageFaults[maxMemoryFrames] = faults;
+    }
+}
