e8647988c17120c09c92535a44f694d65af8c447
Rylei Mindrum <rymindrum@gmail.com>
2024-12-04 21:38:47 -0700

    FIFO

---
+++ src/TaskFIFO.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class TaskFIFO implements Runnable {
+    private final int[] sequence;
+    private final int maxMemoryFrames;
+    private final int[] pageFaults;
+
+    public TaskFIFO(int[] sequence, int maxMemoryFrames, int maxPageReference, int[] pageFaults) {
+        this.sequence = sequence;
+        this.maxMemoryFrames = maxMemoryFrames;
+        this.pageFaults = pageFaults;
+    }
+
+    @Override
+    public void run() {
+        Set<Integer> memory = new HashSet<>();
+        Queue<Integer> fifoQueue = new LinkedList<>();
+        int faults = 0;
+
+        for (int page : sequence) {
+            if (!memory.contains(page)) {
+                faults++;
+                if (memory.size() == maxMemoryFrames) {
+                    memory.remove(fifoQueue.poll());
+                }
+                memory.add(page);
+                fifoQueue.add(page);
+            }
+        }
+        pageFaults[maxMemoryFrames] = faults;
+    }
+}
